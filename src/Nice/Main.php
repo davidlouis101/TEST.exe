ff2cdddb68d3a41690927eea418e60068a12ea60
CrowBaldeYT <54008552+davidlouis101@users.noreply.github.com>
2020-10-24 23:36:57 +0200

    Add files via upload

---
+++ src/Nice/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Nice;
+
+use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends Plugin implements Listener{
+
+ public function onEnable(){
+   $this->getServer()->getLogger()->info(TF::GREEN . "Plugin Aktiviert");
+ }
+ public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) :bool{
+
+     switch($cmd->getName()){
+         
+     case "Nice":
+       if(!$sender->hasPermision("Nice.cmd")){
+         $sender->sendMessage(TF::RED . "Du hast keine Rechte diesen Command zu Benutzen");
+       } else {
+       $sender->sendMessage(TF::GREEN . "Du willst fly aber bekommst es nicht, Dafur bist du nun im Gamemode 1");
+       $sender->setGamemode(1);
+    }
+   }
+   break;
+  }
+  return true;
+}
